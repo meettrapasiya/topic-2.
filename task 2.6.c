3b59c7ab320ca5dc9e5f6cfd3fc549eba1ba80d1
meettrapasiya <trapasiyameet78@gmail.com>
2024-06-20 18:18:57 +0530

    Add files via upload

---
+++ task 2.6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main()
+
+{
+	float redius,area;
+	
+	printf("enter perimeter of circal =");
+	scanf("%f",&redius);
+	
+	printf("enter redius of circal =");
+	scanf("%f",&redius);
+	
+	area = 3.14 * redius * redius;
+	
+	printf("eara of the circal is =%2f,area");
+}
